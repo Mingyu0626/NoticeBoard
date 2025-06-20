8d83aab9b9c8fb18c3428f7c818527dbb6e03617
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-06-20 10:20:37 +0900

    Feat | UI 매니저에 상세보기 및 수정 버튼 온클릭 리스너 구현

--- Assets/01.Script/Post/4. UI/UIManager.cs
+++ Assets/01.Script/Post/4. UI/UIManager.cs
@@ -7,6 +7,13 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
     [SerializeField]
     private UI_PostMenu _postMenu;
 
+    [SerializeField]
+    private UI_PostRead _postRead;
+
+    [SerializeField]
+    private UI_PostModify _postModify;
+
+
     protected override void Awake()
     {
         base.Awake();
@@ -27,5 +34,17 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
         _postMenu.GetComponent<RectTransform>().anchoredPosition = clickedButtonRectTransform.anchoredPosition;
         _postMenu.Refresh(selectedPost);
         _postMenu.gameObject.SetActive(true);
+    }
+
+    public void OnClickReadButton(PostDTO selectedPost)
+    {
+        _postRead.Refresh(selectedPost);
+        _postRead.gameObject.SetActive(true);
+    }
+
+    public void OnClickModifyButton(PostDTO selectedPost)
+    {
+        // _postModify.Refresh(selectedPost);
+        _postModify.gameObject.SetActive(true);
     }
 }
